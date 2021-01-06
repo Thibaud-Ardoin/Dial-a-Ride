c1bd310c554eed91ef67135605a37abab776443a
Rchide <ardoin.thibaud@gmail.com>
2021-01-06 13:24:02 +0100

    Add FC2

--- models.py
+++ models.py
@@ -237,6 +237,26 @@ class FC1(nn.Module):
         x = self.fc2(x)
         return x
 
+class FC2(nn.Module):
+    """ Neural network definition
+    """
+    def __init__(self, size):
+        super(FC2, self).__init__()
+        self.size = size
+        self.fc1 = nn.Linear(in_features=self.size**2, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=2)
+        self.fc3 = nn.Linear(in_features=2, out_features=4)
+        self.fc4 = nn.Linear(in_features=4, out_features=2)
+
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return x
+
 
 if __name__ == '__main__':
 
