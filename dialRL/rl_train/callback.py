91eb56d13d5b4c7a04b971b9a63f60370fc0fe66
Rchide <ardoin.thibaud@gmail.com>
2021-02-18 12:14:54 +0100

    Add callback to RL PPO training

---
+++ dialRL/rl_train/callback.py
@@ -0,0 +1,168 @@
+import os
+import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+import csv
+
+from stable_baselines.common.callbacks import BaseCallback, EvalCallback
+
+# from clearml import Task
+#
+# ask = Task.init(
+#     project_name="DaRP", task_name="1st experiment")
+
+
+class MonitorCallback(EvalCallback):
+    """
+    Callback for saving a model (the check is done every ``check_freq`` steps)
+    based on the training reward (in practice, we recommend using ``EvalCallback``).
+
+    :param check_freq: (int)
+    :param log_dir: (str) Path to the folder where the model will be saved.
+      It must contains the file created by the ``Monitor`` wrapper.
+    :param verbose: (int)
+    """
+    def __init__(self, eval_env, check_freq: int, log_dir: str, n_eval_episodes=5, render=False, verbose=1):
+        super(MonitorCallback, self).__init__(verbose=verbose,
+                                              eval_env=eval_env,
+                                              best_model_save_path=log_dir,
+                                              log_path=log_dir,
+                                              eval_freq=check_freq,
+                                              n_eval_episodes=n_eval_episodes,
+                                              deterministic=True,
+                                              render=render)
+        self.render = render
+        self.verbose = verbose
+        self.env = eval_env
+        self.check_freq = check_freq
+        self.log_dir = log_dir
+        self.save_path = os.path.join(log_dir, 'best_model')
+        self.best_mean_reward = -np.inf
+        self.sacred = None
+        self.statistics = {
+            'step_reward': [],
+            'reward': [],
+            'std_reward': [],
+            'duration': []
+        }
+
+
+    def _init_callback(self) -> None:
+        # Create folder if needed
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_training_start(self) -> None:
+        """
+        This method is called before the first rollout starts.
+        """
+        pass
+
+    def _on_training_end(self) -> None:
+        """
+        This event is triggered before exiting the `learn()` method.
+        """
+        pass
+
+
+    def plot_statistics(self, show=False):
+        # Print them
+        if self.verbose:
+            print('\t ->[Epoch %d]<- mean episodic reward: %.3f' % (self.num_timesteps + 1, self.statistics['reward'][-1]))
+            print('\t * Mean duration : %0.3f' % (self.statistics['duration'][-1]))
+
+        # Create plot of the statiscs, saved in folder
+        colors = [plt.cm.tab20(0),plt.cm.tab20(1),plt.cm.tab20c(2),
+                  plt.cm.tab20c(3), plt.cm.tab20c(4),
+                  plt.cm.tab20c(5),plt.cm.tab20c(6),plt.cm.tab20c(7)]
+        fig, (axis) = plt.subplots(1, len(self.statistics), figsize=(20, 10))
+        fig.suptitle(' - PPO Training: ' + self.log_dir)
+        for i, key in enumerate(self.statistics):
+            # Sacred (The one thing to keep here)
+            if self.sacred :
+                self.sacred.log_scalar(key, self.statistics[key][-1], len(self.statistics[key]))
+            axis[i].plot(self.statistics[key], color=colors[i])
+            axis[i].set_title(' Plot of ' + key)
+        if show :
+            fig.show()
+        fig.savefig(self.log_dir + '/result_figure.jpg')
+        fig.clf()
+        plt.close(fig)
+
+        # Save the statistics as CSV file
+        if not self.sacred:
+            try:
+                with open(self.log_dir + '/statistics.csv', 'w') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=self.statistics.keys())
+                    writer.writeheader()
+                    # for key in statistics
+                    writer.writerow(self.statistics)
+            except IOError:
+                print("I/O error")
+
+
+    def save_images(self, images, rewards, txt='test'):
+        ''' Saving some examples of input -> output to see how the model behave '''
+        print(' - Saving some examples - ')
+        number_i = min(len(images), 50)
+        plt.figure()
+        fig, axis = plt.subplots(number_i, 2, figsize=(10, 50)) #2 rows for input, output
+        fig.tight_layout()
+        fig.suptitle(' - examples of network - ')
+        for i in range(min(self.batch_size, number_i)):
+            input_map = indices2image(data[0][i], self.image_size)
+            axis[i, 0].imshow(input_map)
+            im = indice_map2image(outputs[i], self.image_size).cpu().numpy()
+            normalized = (im - im.min() ) / (im.max() - im.min())
+            axis[i, 1].imshow(normalized)
+        img_name = self.path_name + '/example_' + str(self.num_timesteps) + '.png'
+        plt.savefig(img_name)
+        plt.close()
+        if self.sacred :
+            self.sacred.add_artifact(img_name, content_type='image')
+
+    def save_gif(self, observations, rewards):
+        observations = np.array(observations)[:10]
+        save_name = self.log_dir + '/example' + str(self.num_timesteps) + '.gif'
+        images = observations.astype(np.uint8) #[np.array(img) for i, img in enumerate(images)]
+        imageio.mimsave(save_name, images, fps=1)
+
+
+    def _on_step(self) -> bool:
+        """
+        In addition to EvalCallback we needs
+        Examples of eviroonment elements -> Save them as gif for exemple
+        Statistics to save -> save as plot and in database
+            -> reward, length, loss, additional metrics (accuraccy, best move ?)
+        """
+        super(MonitorCallback, self)._on_step()
+        if self.num_timesteps % self.check_freq == 0 :
+
+            observations = []
+            episode_rewards, episode_lengths = [], []
+            for i in range(self.n_eval_episodes):
+                obs = self.env.reset()
+                observations.append(obs.numpy())
+                done, state = False, None
+                episode_rewards.append(0.0)
+                episode_lengths.append(0)
+                while not done:
+                    action, state = self.model.predict(obs, state=state, deterministic=self.deterministic)
+                    new_obs, reward, done, _info = self.env.step(action)
+
+                    obs = new_obs
+                    observations.append(obs.numpy())
+                    episode_rewards[-1] += reward
+                    episode_lengths[-1] += 1
+                    if self.render:
+                        self.env.render()
+
+                self.save_gif(observations, episode_rewards)
+
+            self.statistics['reward'].append(np.mean(episode_rewards))
+            self.statistics['std_reward'].append(np.std(episode_rewards))
+            self.statistics['step_reward'].append(np.mean([episode_rewards[i]/episode_lengths[i] for i in range(len(episode_lengths))]))
+            self.statistics['duration'].append(np.mean(episode_lengths))
+            self.plot_statistics()
+
+            return True
--- dialRL/rl_train/train_ppo.py
+++ dialRL/rl_train/train_ppo.py
@@ -1,27 +1,37 @@
 import gym
 import logging
 import os
+import imageio
 
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
+from stable_baselines.common.callbacks import EvalCallback
 
 from dialRL.rl_train.environments import DarEnv
 from dialRL.models import DQN
 from dialRL.utils import get_device
+from dialRL.rl_train.callback import MonitorCallback
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 logging.getLogger('tensorflow').setLevel(logging.FATAL)
 
+
 # policy_model = DQN
-env = DarEnv(size=6, target_population=5, driver_population=1)
+env = DarEnv(size=4, target_population=5, driver_population=1)
 device = get_device()
 
 print('Env action space :', env.action_space)
 
+monitor = MonitorCallback(eval_env=env,
+                          check_freq=1000,
+                          log_dir='./data/rl_experiments',
+                          n_eval_episodes=5,
+                          verbose=1)
 
-model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=100000)#25000)
+
+model = PPO2(MlpPolicy, env, verbose=0)
+model.learn(total_timesteps=10000, callback=monitor)
 model.save("Dar_test")
 
 del model # remove to demonstrate saving and loading
