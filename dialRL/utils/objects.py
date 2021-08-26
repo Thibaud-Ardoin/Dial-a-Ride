d603d9707afb5f0d03bff6038c6a0bb3213187f3
Rchide <ardoin.thibaud@gmail.com>
2021-08-26 17:21:48 +0200

    Add dilation on global scale in tame and space + Fix enormous fail in augmentation

--- dialRL/utils/objects.py
+++ dialRL/utils/objects.py
@@ -33,7 +33,7 @@ class SupervisionDataset(Dataset):
         return len(self.data)
 
 
-    def pos_augmentation(self, positions):
+    def pos_augmentation(self, positions, rand_vect, dilate=None):
         ''' Transposing all popsition information by a 2D vectors in [-1,1]²
             In addition the 2D points are rotated by a random value [-pi, pi]
         '''
@@ -42,13 +42,15 @@ class SupervisionDataset(Dataset):
         get_rot = lambda theta: torch.tensor([[torch.cos(theta*np.pi), -torch.sin(theta*np.pi)],
                                 [torch.sin(theta*np.pi), torch.cos(theta*np.pi)]]).double()
 
-        rand_vect = torch.rand(2 + 1) * 2 - 1
-        rot = get_rot(torch.tensor(0))#rand_vect[-1])
-        trsp = rand_vect[:-1]
+        rand_vect = rand_vect * 2 - 1
+        rot = get_rot(torch.tensor(rand_vect[2]))
+        trsp = rand_vect[:2]
+        mirror = rand_vect[3]
 
-        comp_aug2d = lambda pos: torch.matmul(torch.from_numpy(pos), rot) + trsp
-        comp_aug4d = lambda pos: torch.cat([torch.matmul(torch.from_numpy(pos[:2]), rot) + trsp,
-                                            torch.matmul(torch.from_numpy(pos[2:]), rot) + trsp])
+        comp_aug2d = lambda pos: (torch.matmul(pos, rot) + trsp) * dilate if torch.is_tensor(pos) else (torch.matmul(torch.from_numpy(pos), rot) + trsp) * dilate
+        comp_aug4d = lambda pos: torch.cat([torch.matmul(pos[:2], rot) + trsp,
+                                            torch.matmul(pos[2:], rot) + trsp]) * dilate if torch.is_tensor(pos) else torch.cat([torch.matmul(torch.from_numpy(pos[:2]), rot) + trsp,
+                                                                                                                                 torch.matmul(torch.from_numpy(pos[2:]), rot) + trsp]) * dilate
 
         depot = comp_aug2d(depot)
         targets = list(map(comp_aug4d, targets))
@@ -57,20 +59,21 @@ class SupervisionDataset(Dataset):
         return [depot, targets, drivers]
 
 
-    def time_augmentation(self, time):
+    def time_augmentation(self, time, rand_vect, dilate=None):
         ''' Shifting all time information by a random amount
             This transposition value is in [0, 10]
             half of the time it's rounded to an integer
         '''
         current, targets, drivers = time
+        rand1, rand2 = rand_vect
 
-        rand1, rand2 = torch.rand(2)
+        # rand1, rand2 = torch.rand(2)
         trsp = rand1 * 10
         if rand2 > 0.5:
             trsp = torch.round(trsp)
 
-        aug4d = lambda tem: torch.from_numpy(tem) + trsp
-        aug1d = lambda tem: tem + trsp
+        aug4d = lambda tem: (tem + trsp) * dilate if torch.is_tensor(tem) else (torch.from_numpy(tem) + trsp) * dilate
+        aug1d = lambda tem: (tem + trsp) * dilate
 
         current = aug1d(current)
         targets = list(map(aug4d, targets))
@@ -78,16 +81,25 @@ class SupervisionDataset(Dataset):
 
         return [current, targets, drivers]
 
+    def distance_dilatation(self, targets, dilate):
+        dilat_distance = lambda t: [t[0], t[1], t[2], t[3]*dilate, t[4]*dilate]
+        targets = list(map(dilat_distance, targets))
+        return targets
+
+
 
     def __getitem__(self, idx):
         """ simple idx """
         obs, sup = self.data[idx]
         world, targets, drivers, positions, time_constraints = obs
         if hasattr(self, 'augment') and self.augment is not None and self.augment:
-            positions = self.pos_augmentation(positions)
-            time_constraints = self.time_augmentation(time_constraints)
+            rand_vect = torch.rand(2+4+1).double()     # 2 for time (1round, 1shift) + 4 for pos (2transp vect, 1rot, 1miror)  + 1dilation
+            dilate = 1 + (rand_vect[-1] * 2 - 1)/10
+            positions = self.pos_augmentation(positions, rand_vect[2:-1], dilate)
+            time_constraints = self.time_augmentation(time_constraints, rand_vect[:2], dilate)
+            targets = self.distance_dilatation(targets, dilate)
 
-        return self.data[idx]
+        return [world, targets, drivers, positions, time_constraints], sup
 
 class objdict(dict):
     def __getattr__(self, name):
